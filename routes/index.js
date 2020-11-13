7cf631615c4ae841daa567fe7a2aea18155d1a17
Simranpreet Kaur <simarkaur3320@gmail.com>
2020-11-12 19:55:39 -0500

    Adding 4 more views using hbs

--- routes/index.js
+++ routes/index.js
@@ -8,5 +8,25 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Jewels House' });
 });
 
+/* GET About page. */
+router.get('/about', function (req, res, next) {
+    res.render('about')
+});
+
+/* GET Projects page. */
+router.get('/projects', function (req, res, next) {
+    res.render('projects')
+});
+
+/* GET Services page. */
+router.get('/services', function (req, res, next) {
+    res.render('services')
+});
+
+/* GET Contact page. */
+router.get('/contact', function (req, res, next) {
+    res.render('contact')
+});
+
 //express this file as public
 module.exports = router;
